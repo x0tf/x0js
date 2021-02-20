6c6f0b16576eb165eb7248e830c139c6dd93c753
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-02-21 00:02:56 +0100

    refactor(*): finished element methods
    elemet specific methods like create and delete implemented

--- src/elements/elements.ts
+++
@@ -1,9 +0,0 @@
-import namespace from './NamespaceManager';
-import redirect from './redirect';
-import paste from './paste';
-
-export default class elements {
-    static namespace = namespace;
-    static redirect = redirect;
-    static paste = paste;
-}
